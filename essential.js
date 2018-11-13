da8f7f1af01ff66ac9b91a68064e0a91395f0916
Mika Vallin <mika.vallin@gmail.com>
2018-11-13 09:25:40 +0200

    essentials.js added
    Here I introduce header, navigation and footer which are common to all pages.

---
+++ essential.js
@@ -0,0 +1,6 @@
+document.getElementById("pageHeader").innerHTML = "<h1>Site Title</h1>";
+document.getElementById("pageNavi").innerHTML = "<ul id='navLinks'>"
+  + "<li><a href='index.html'>Home</a></li>"
+  + "<li><a href='news.html'>News</a></li>"
+  + "<li><a href='about.html'>About</a></li>";
+document.getElementById("pageFooter").innerHTML = "<p>This is footer</p>";
