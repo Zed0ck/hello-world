246bc83a5b7cce78ead9928d6d9671f35c37ec8f
Zed0ck <mika.vallin@gmail.com>
2018-11-13 20:51:30 +0200

    navigation and footer is now loaded from essential.js

--- essential.js
+++ essential.js
@@ -1,6 +1,10 @@
-document.getElementById("pageHeader").innerHTML = "<h1>Site Title</h1>";
-document.getElementById("pageNavi").innerHTML = "<ul id='navLinks'>"
+document.getElementById("pageNavi").innerHTML = "<ul>"
   + "<li><a href='index.html'>Home</a></li>"
   + "<li><a href='news.html'>News</a></li>"
   + "<li><a href='about.html'>About</a></li>";
-document.getElementById("pageFooter").innerHTML = "<p>This is footer</p>";
+document.getElementById("pageFooter").innerHTML = "<div class='col-sm-6'>"
+  + "<p>You can find me on GitHub.</p>"
+  + "</div>"
+  + "<div class='col-sm-6'>"
+  + "<p>You also find me in Twitter and Instagram if you search hard enough.</p>"
+  + "</div>";
