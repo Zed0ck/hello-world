afe34c180200cede08ff92b0429ae6e387344e0c
Zed0ck <mika.vallin@gmail.com>
2018-11-14 12:53:07 +0200

    Adding my first game done with Phaser and embedded it to my site.

--- essential.js
+++ essential.js
@@ -1,7 +1,8 @@
 document.getElementById("pageNavi").innerHTML = "<ul>"
   + "<li><a href='index.html'>Home</a></li>"
   + "<li><a href='news.html'>News</a></li>"
-  + "<li><a href='about.html'>About</a></li>";
+  + "<li><a href='about.html'>About</a></li>"
+  + "<li><a href='game1.html'>Game 1</a></li>";
 document.getElementById("pageFooter").innerHTML = "<div class='col-sm-6'>"
   + "<p>You can find me on GitHub.</p>"
   + "</div>"
